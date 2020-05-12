060e24fe4c7d6df2bffad1aee2cd94822e080ba0
Lautaro Bertuzzi <lautarobertuzzi@MacBook-Pro-de-Lautaro.local>
2020-05-12 15:22:31 -0300

    Fixed bundler version

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,12 @@
 require File.expand_path('../boot', __FILE__)
-require 'rails/all'
+require "active_model/railtie" 
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "active_job/railtie" # Only for Rails >= 4.2
+require "action_cable/engine" # Only for Rails >= 5.0
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
